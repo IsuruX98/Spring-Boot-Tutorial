1efa20f42325550f773ab8dc65f9f38712c463c0
IT21169380 <isurusanka98@gmail.com>
2023-05-26 16:06:05 +0530

    employee putmapping created

--- employeeManagementSystem/src/main/java/com/example/employeemanagementsystem/controller/EmployeeController.java
+++ employeeManagementSystem/src/main/java/com/example/employeemanagementsystem/controller/EmployeeController.java
@@ -7,10 +7,7 @@ import com.example.employeemanagementsystem.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "api/v1/employee")
@@ -35,6 +32,40 @@ public class EmployeeController {
         responceDTO.setCode(VarList.RSP_DUPLICATED);
         responceDTO.setMessage("employee registered");
         responceDTO.setObject(employeeDTO);
+
+        return new ResponseEntity(responceDTO, HttpStatus.BAD_REQUEST);
+      } else {
+        responceDTO.setCode(VarList.RSP_FAIL);
+        responceDTO.setMessage("Error");
+        responceDTO.setObject(null);
+
+        return new ResponseEntity(responceDTO, HttpStatus.BAD_REQUEST);
+      }
+
+    } catch (Exception ex) {
+      responceDTO.setCode(VarList.RSP_ERROR);
+      responceDTO.setMessage(ex.getMessage());
+      responceDTO.setObject(null);
+
+      return new ResponseEntity(responceDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @PutMapping(value = "/updateEmployee")
+  public ResponseEntity updateEmployee(@RequestBody EmployeeDTO employeeDTO) {
+    try {
+      String res = employeeService.updateEmployee(employeeDTO);
+
+      if (res.equals("00")) {
+        responceDTO.setCode(VarList.RSP_SUCCESS);
+        responceDTO.setMessage("Success");
+        responceDTO.setObject(employeeDTO);
+
+        return new ResponseEntity(responceDTO, HttpStatus.ACCEPTED);
+      } else if (res.equals("01")) {
+        responceDTO.setCode(VarList.RSP_NO_DATA_FOUND);
+        responceDTO.setMessage("not a registered employee");
+        responceDTO.setObject(null);
 
         return new ResponseEntity(responceDTO, HttpStatus.BAD_REQUEST);
       } else {
